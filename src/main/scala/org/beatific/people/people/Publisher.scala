170c34e97d0bf334ae0e2f9a0f3b0f0f29bc319f
P067880 <P067880@SKT-RN29452693.SKCC.NET>
2018-04-09 17:21:28 +0900

    publish to all observers

--- src/main/scala/org/beatific/people/people/Publisher.scala
+++ src/main/scala/org/beatific/people/people/Publisher.scala
@@ -6,22 +6,16 @@ import org.beatific.people.utils.reflection.ClassSupports
 
 object Publisher {
 
-  var observers: Map[Class[_], Observer[_]] = Map()
+  var observers: Map[Class[_], List[Observer[_]]] = Map()
   
-//  ClassSupports.findClassBySuperClass("org.beatific", classOf[Observer[_ <: Event]]) foreach {
-//    observer => {
-//      val ob = observer.newInstance()
-//      observers += (ob.genericType.asInstanceOf[Class[_ <: Event]] -> ob.asInstanceOf[Observer[_ <: Event]])
-//    }
-//  }
-
   def +[T <: Event](observer: Observer[T]) {
-    observers += (observer.genericType.asInstanceOf[Class[T]] -> observer.asInstanceOf[Observer[T]])
+    val t = observer.genericType.asInstanceOf[Class[T]]
+    observers +=  (observer.genericType.asInstanceOf[Class[T]] -> (observers.getOrElse(t, Nil) :+ observer.asInstanceOf[Observer[T]]))
   }
 
   def >>[T](event: T) {
     observers.get(event.getClass()) match {
-      case Some(observer) => observer.asInstanceOf[Observer[T]].receive(event)
+      case Some(observers) => observers.foreach(_.asInstanceOf[Observer[T]].receive(event))
       case None           =>
 
     }
